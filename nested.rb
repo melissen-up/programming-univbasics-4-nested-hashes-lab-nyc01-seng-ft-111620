911b125743f94b40c1c7ccff12755a340eaf6f9a
Melissa F <melissa.faulner@gmail.com>
2020-11-02 05:39:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,10 +15,11 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
+  programmer_hash[]
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
