2da9cdf4b28a010bc957193130086431236f2f37
Melissa F <melissa.faulner@gmail.com>
2020-11-02 05:40:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,9 +19,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-  programmer_hash[:alan_kay][:known_for]
-	
-	programmer_hash = 
+ 	programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
       }
     }
 
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
